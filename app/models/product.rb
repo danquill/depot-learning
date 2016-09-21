810a2378c3b4087dfe3312c5ca110b9a52dea88a
Daniel Quillen <dquillen@taphere.com>
2016-09-21 15:36:17 -0400

    Finished the index chapter

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: "must be a url ending in gif, jpg, or png."
     }
+
+    def self.latest
+      Product.order(:updated_at).last
+    end
 end
