f1111161dde50e0bcbd207c2a5d0093c0f3ca3b3
Daniel Quillen <dquillen@taphere.com>
2016-09-22 11:11:54 -0400

    Added initial cart functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,22 @@ class Product < ActiveRecord::Base
     message: "must be a url ending in gif, jpg, or png."
     }
 
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
     def self.latest
       Product.order(:updated_at).last
+    end
+
+    private
+
+    #ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line items present')
+        return false
+      end
     end
 end
